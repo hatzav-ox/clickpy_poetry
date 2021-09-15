d4ab7018a10ec67f5071c404dc37fbb4c60ddfdb
fitzypop <fitzypop@pm.me>
2021-09-14 19:56:04 -0600

    Added coverage methods for poetry scripts

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,3 +7,10 @@ def run():
     import os
 
     os.system("pytest tests/ -v --cov=clickpy/")
+
+
+def coverage():
+    """Run coverage generation and report."""
+    import os
+
+    os.system("coverage html && open htmlcov/index.html")
